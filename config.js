2e844eb906451e37d01d782dd453e3526bbf21f3
huk250699 <87457168+huk250699@users.noreply.github.com>
2021-11-08 08:38:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Hằng';
+const giftUrl = 'https://www.facebook.com/';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng Hằng';
+const contentCard = ' Mừng sinh nhật cô gái tháng 11. Tuổi mới, chúc em luôn bình an, hạnh phúc và sớm chinh phục những nấc thang trong đời';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
